f14fa536e9529f042edb551de9433ae062545965
parsoli83 <mr.parsoli@gmail.com>
2024-02-11 13:04:32 +0330

    added email auth 2 min

--- app.py
+++ app.py
@@ -1,73 +1,16 @@
-
+import datetime
+from email_ver import email_ver
 import os
 from time import time
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
 import re
-import csv
-import datetime
-import pytz
-import requests
-import subprocess
-import urllib
-import uuid
-from functools import wraps
-import smtplib
-from email.message import *
-from random import randint
 
-
-def email_ver(user_email):
-    def random_pass(num = "",depth = 4):
-        if depth==0:
-            return num
-        return random_pass(num+str(randint(1,9)),depth-1)
-    
-    my_email = "parsoli.pardakht@gmail.com"
-    my_pass = "kcks uqmv dbha ytyx"
-    password = random_pass()
-    msg = EmailMessage()
-    msg["to"] = user_email
-    msg["from"] = my_email
-    msg["subject"] = "Password"
-    msg.set_content(password)
-    with smtplib.SMTP_SSL("smtp.gmail.com",465) as current:
-        current.login(my_email,my_pass)
-        current.send_message(msg)
-    return password
-
-
-
-
-def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
-
-
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
+verified=[False]
+l_ev=[]
 
 
 
@@ -98,6 +41,28 @@ def after_request(response):
     return response
 
 
+@app.route("/email_auth", methods=["GET", "POST"])
+def email_auth():
+    print("_"*30,"email_auth","_"*30)
+    
+    if request.method == "POST" and request.form.get("email_ver"):
+        ver_code=l_ev[3]
+        print("ver-code: ",ver_code)
+        
+        inp = request.form.get("email_ver")
+        if datetime.datetime.now().timestamp()-l_ev[-1]>120:
+            return apology("took more than 2 min",403)
+        if str(inp).strip()==ver_code:
+            verified[0]=True
+            return redirect(f"{l_ev[4]}")
+        else:
+            return apology("email verification code is incorrect",403)
+    else:
+        return render_template("email_auth.html")
+
+
+
+
 @app.route("/")
 @login_required
 def index():
@@ -106,11 +71,18 @@ def index():
     #JOIN cards ON users.id=cards.owner_id JOIN pn ON cards.owner_id=pn.user_id
     l_all = db.execute(f"SELECT * FROM users")
     history = db.execute(f"SELECT * FROM history WHERE user_id = {session['user_id']}")
+    if len(history)>6:
+        history=history[-5:]
+        history.reverse()
+    print("/"*30,"history","/"*30)
+    print(history)
+
+    
     print(l_all)
-    print(history)
+    
     l_cards = db.execute(f"SELECT card,cash FROM cards WHERE owner_id={session['user_id']}")
-    history =  db.execute(f"SELECT * FROM history WHERE user_id={session['user_id']}")
-    print(history)
+    
+    
     l_pn = db.execute(f"SELECT * FROM pn WHERE user_id={session['user_id']}")
     print(l_cards,l_pn)
     if len(l_cards)==0:
@@ -145,37 +117,56 @@ def addcard():
 @login_required
 def charge():
     print("_"*30,"charge","_"*30)
-    l_cards = db.execute(f"SELECT card,cash FROM cards WHERE owner_id={session['user_id']}")
+    if verified[0]:
+        #card_
+        #pn_
+        #amount
+        db.execute(f"UPDATE cards SET cash={l_ev[0]['cash']-l_ev[2]} WHERE card = '{l_ev[0]['card']}'")
+        print(2)
+        db.execute(f"UPDATE pn SET charge= {l_ev[1]['charge']+l_ev[2]} WHERE phone_number = '{l_ev[1]['phone_number']}'")
+        print(3)
+        db.execute(f"INSERT INTO history(user_id,type,sender,reciever,amount) VALUES({session['user_id']},'charge','{l_ev[0]['card']}','{l_ev[1]['phone_number']}',{l_ev[2]})")
+        l_ev.clear()
+        return redirect("/")
+    l_cards = db.execute(f"SELECT card,cash,pass FROM cards WHERE owner_id={session['user_id']}")
     l_pn = db.execute(f"SELECT * FROM pn WHERE user_id={session['user_id']}")
-    print(l_cards,l_pn)
     if request.method == "POST":
+        verified[0]=False
         pn = request.form.get("phone_number")
         card = request.form.get("card")
         amount = request.form.get("amount")
+        ps = request.form.get("ps")
+        email = db.execute(f"SELECT * FROM users WHERE id={session['user_id']}")[0]["email"]
         print(f"card: {card}")
         print(f"pn: {pn}")
         print(f"amount: {amount}")
+        print(f"pass: {ps}")
+        print(f"email: {email}")
         
-        if not card:
+        if not card or not pn or not amount:
             return apology("dont leave fields blank")
-        
+      
         try:
             amount = int(amount)
             if amount<1:
                 return apology("cant buy negative charge")
             card_ = l_cards[int(card)]
             pn_= l_pn[int(pn)]
-            print(card_)
-            print(pn_)
-            print(1)
+            print(ps)
+            print(card_["pass"])
+            if str(ps)!=str(card_["pass"]):
+                return apology("invalid card pass")
             if int(card_["cash"])<amount:
                 return apology("not enough money")
-            db.execute(f"UPDATE cards SET cash={card_['cash']-amount} WHERE card = '{card_['card']}'")
-            print(2)
-            db.execute(f"UPDATE pn SET charge= {pn_['charge']+amount} WHERE phone_number = '{pn_['phone_number']}'")
-            print(3)
-            db.execute(f"INSERT INTO history(user_id,type,sender,reciever,amount) VALUES({session['user_id']},'charge','{card_['card']}','{pn_['phone_number']}',{amount})")
-            return redirect("/")
+            l_ev.append(card_)
+            l_ev.append(pn_)
+            l_ev.append(amount)
+            l_ev.append(email_ver(str(email).strip()))
+            print("code: ",l_ev[-1])
+            l_ev.append("/charge")
+            l_ev.append(datetime.datetime.now().timestamp())
+            print(l_ev)
+            return redirect("/email_auth")
         except:
             return apology("meowmeowmeow")
     return render_template("charge.html",l_pn=l_pn,l_cards=l_cards)
@@ -191,40 +182,68 @@ def charge():
 def transfer():
     print("_"*30,"transfer","_"*30)
     l_cards = db.execute(f"SELECT card,cash FROM cards")
-    l_mine = db.execute(f"SELECT card,cash FROM cards WHERE owner_id={session['user_id']}")
+    l_mine = db.execute(f"SELECT card,cash,pass FROM cards WHERE owner_id={session['user_id']}")
     
     print(l_cards)
     print(l_mine)
+    if verified[0]:
+        #send
+        #reci
+        #cash
+        db.execute(f"UPDATE cards SET cash={l_ev[0]['cash']-l_ev[2]} WHERE card = '{l_ev[0]['card']}'")
+        print(2)
+        db.execute(f"UPDATE cards SET cash={l_ev[1]['cash']+l_ev[2]} WHERE card = '{l_ev[1]['card']}'")            
+        print(3)
+        db.execute(f"INSERT INTO history(user_id,type,sender,reciever,amount) VALUES({session['user_id']},'transfer','{l_ev[0]['card']}','{l_ev[1]['card']}',{l_ev[2]})")
+        l_ev.clear()
+        return redirect("/")
     if request.method == "POST":
+        verified[0]=False
         sender = request.form.get("sender")
         reciever = request.form.get("reciever")
         amount = request.form.get("amount")
+        ps = request.form.get("ps")
+        email = db.execute(f"SELECT * FROM users WHERE id={session['user_id']}")[0]["email"]
         print(f"sender: {sender}")
         print(f"reciever: {reciever}")
         print(f"amount: {amount}")
+        print(f"ps: {ps}")
+        print(f"email: {email}")
+
         
         if not sender or not reciever or not amount:
             return apology("dont leave fields blank")
         
         try:
+            print(0)
             amount = int(amount)
             if amount<1:
                 return apology("cant buy negative charge")
+            print(0.1)
             send = l_mine[int(sender)]
-            reci = l_cards[int(reciever)]
-            print("send")
             print(send)
-            print("reci")
-            print(reci)
+            print(0.12)
+            if str(send["pass"])!=str(ps):
+                return apology("invalid user/pass")
+            print(0.2)
+            reci = l_cards[int(reciever)]
             print(1)
+
             if int(send["cash"])<amount:
                 return apology("not enough money")
-            db.execute(f"UPDATE cards SET cash={send['cash']-amount} WHERE card = '{send['card']}'")
+            if str(ps)!=str(send["pass"]):
+                return apology("invalid card pass")
             print(2)
-            db.execute(f"UPDATE cards SET cash={reci['cash']+amount} WHERE card = '{reci['card']}'")            
+            l_ev.append(send)
+            l_ev.append(reci)
+            l_ev.append(amount)
+            l_ev.append(email_ver(str(email).strip()))
+            print("code: ",l_ev[-1])
+            l_ev.append("/transfer")
+            l_ev.append(datetime.datetime.now().timestamp())
+            print(l_ev)
             print(3)
-            db.execute(f"INSERT INTO history(user_id,type,sender,reciever,amount) VALUES({session['user_id']},'transfer','{send['card']}','{reci['card']}',{amount})")
-            return redirect("/")
+            return redirect("/email_auth")
         except:
             print("heeeeeb")
             return apology("meowmeowmeow",400)
@@ -279,13 +298,15 @@ def addpn():
             return apology("meowmeowmeow")
     return render_template("addpn.html")
 
-l_ev=[]
+
 @app.route("/email_ver", methods=["GET", "POST"])
 def email_ver_check():
-    
+    print("_"*30,"email_ver","_"*30)
     if request.method == "POST" and request.form.get("email_ver"):
         print(l_ev)
         user_info,ver_code = l_ev[0],l_ev[1]
+        print("user_info: ",user_info)
+        print("ver_code: ",ver_code)
         l_ev.clear()
         inp = request.form.get("email_ver")
         if inp==ver_code:
@@ -372,7 +393,7 @@ def register():
             pn = request.form.get("phone_number")
             print(request.form)
             # Ensure username was submitted
-            """
+            
             if not user_name:
                 return apology("must provide username", 400)
             elif user_name in user_names:
@@ -390,9 +411,7 @@ def register():
                 return apology("invalid email")
             elif not (pn[0]=="0" and pn[1]=="1" and len(pn)==11):
                 return apology("invalid phone number")
-            """
-            if 1==2:
-                ...
+            
             else:
                 pass_hash  = generate_password_hash(pass_word)
                 card_pass_hash = generate_password_hash(card_pass)
@@ -439,8 +458,8 @@ def str_xor(s1, s2):
 """
 usage:
 
-encoded = str_xor(message, somecode)
-decoded = str_xor(encoded, somecode)
+encoded = str_xor(message, passcode)
+decoded = str_xor(encoded, passcode)
 
 """
 
